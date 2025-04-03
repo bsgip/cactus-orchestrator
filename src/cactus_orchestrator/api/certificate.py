b3e481ec52bd9473bf069aca398726c1c9308b05
Mustafa Habib <mustafa.habib@anu.edu.au>
2025-04-03 16:23:10 +1100

    wip

--- src/cactus_orchestrator/api/certificate.py
+++ src/cactus_orchestrator/api/certificate.py
@@ -13,6 +13,7 @@ from cactus_orchestrator.cache import AsyncCache, ExpiringValue
 from cactus_orchestrator.crud import select_user, upsert_user
 from cactus_orchestrator.k8s.certificate.create import generate_client_p12
 from cactus_orchestrator.k8s.certificate.fetch import fetch_certificate_key_pair, fetch_certificate_only
+from cactus_orchestrator.manager.certificate import CertificateManager
 from cactus_orchestrator.schema import UserContext
 from cactus_orchestrator.settings import TEST_CLIENT_P12_PASSWORD, CactusOrchestratorException, main_settings
 
@@ -26,47 +27,24 @@ MEDIA_TYPE_P12 = "application/x-pkcs12"
 MEDIA_TYPE_CA_CRT = "application/x-x509-ca-cert"
 
 
-async def update_ca_certificate_cache(_: Any) -> dict[str, ExpiringValue[x509.Certificate]]:
-    cert = await fetch_certificate_only(main_settings.tls_ca_certificate_generic_secret_name)
-
-    return {_ca_crt_cachekey: ExpiringValue(expiry=cert.not_valid_after_utc, value=cert)}
-
-
-# NOTE: do not log.
-_ca_crt_cachekey = ""
-_ca_crt_cache = AsyncCache(update_fn=update_ca_certificate_cache, force_update_delay_seconds=60)
-
-
-async def create_client_cert_binary(user_context: UserContext) -> tuple[bytes, bytes]:
-    ca_cert, ca_key = await fetch_certificate_key_pair(main_settings.tls_ca_tls_secret_name)  # TODO: cache maybe?
-
-    # create client certificate
-    client_p12, client_cert = generate_client_p12(
-        ca_cert=ca_cert,
-        ca_key=ca_key,
-        client_common_name=user_context.subject_id,
-        p12_password=TEST_CLIENT_P12_PASSWORD.get_secret_value(),
-    )
-    return client_p12, client_cert.public_bytes(encoding=serialization.Encoding.DER)
-
-
 @router.get(
     "/certificate",
     response_class=Response,
     responses={HTTPStatus.OK: {"content": {MEDIA_TYPE_P12: {}}}},
 )
-async def fetch_existing_certificate(
+async def get_certificate(
     user_context: Annotated[UserContext, Depends(jwt_validator.verify_jwt_and_check_scopes({AuthScopes.user_all}))],
 ) -> Response:
 
-    # get user with p12
-    user = await select_user(db.session, user_context, with_p12=True)
+    try:
+        client_p12 = CertificateManager.fetch_existing_certificate_p12(db.session, user_context)
 
-    if user is None:
-        raise HTTPException(status_code=HTTPStatus.CONFLICT, detail="No certificate exists, please register.")
+    except CactusOrchestratorException as exc:
+        logger.debug(exc)
+        raise HTTPException(status_code=HTTPStatus.CONFLICT)
 
     return Response(
-        content=user.certificate_p12_bundle,
+        content=client_p12,
         media_type=MEDIA_TYPE_P12,
     )
 
@@ -85,13 +63,8 @@ async def fetch_existing_certificate(
 async def create_user_certificate(
     user_context: Annotated[UserContext, Depends(jwt_validator.verify_jwt_and_check_scopes({AuthScopes.user_all}))],
 ) -> Response:
-    # create certs
-    client_p12, client_x509_der = await create_client_cert_binary(user_context)
 
-    # insert or update user with new cert
-    _ = await upsert_user(db.session, user_context, client_p12=client_p12, client_x509_der=client_x509_der)
-
-    await db.session.commit()
+    client_p12 = await CertificateManager.create_user_certificate(db.session, user_context)
 
     return Response(
         content=client_p12,
@@ -106,15 +79,12 @@ async def create_user_certificate(
     response_class=Response,
     responses={HTTPStatus.OK: {"content": {MEDIA_TYPE_CA_CRT: {}}}},
 )
-async def fetch_current_certificate_authority_der(
+async def get_certificate_authority(
     _: Annotated[UserContext, Depends(jwt_validator.verify_jwt_and_check_scopes({AuthScopes.user_all}))],
 ) -> Response:
 
-    # fetch ca
-    ca_cert = await _ca_crt_cache.get_value(None, _ca_crt_cachekey)
-
-    if ca_cert is None:
-        raise CactusOrchestratorException("CA certificate not found.")
+    # fastapi should raise 500 if this fails
+    ca_cert = await CertificateManager.fetch_current_certificate_authority_der()
 
     return Response(
         content=ca_cert.public_bytes(serialization.Encoding.DER),
