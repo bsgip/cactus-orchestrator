97088639033108e06555c3d2faed751b7fc18403
Mike Turner <mike.turner@anu.edu.au>
2026-05-07 10:15:48 +0800

    Add compliance request data to fixture for testing

--- tests/data/compliance_config.sql
+++ tests/data/compliance_config.sql
@@ -7,7 +7,7 @@ VALUES ('user1', 'https://test-cactus-issuer.example.com', 64, 'user1@cactus.exa
 
 -- Run Groups
 INSERT INTO run_group (user_id, name, csip_aus_version, is_device_cert, certificate_pem, certificate_generated_at, certificate_id)
-VALUES (2, 'device-1', 'v1.2', TRUE, E'\\x01', '2023-01-01T00:01:00Z', 11);
+VALUES (2, 'device-1', 'v1.2', TRUE, '\x01', '2023-01-01T00:01:00Z', 11);
 INSERT INTO run_group (user_id, name, csip_aus_version, is_device_cert, certificate_pem, certificate_generated_at, certificate_id)
 VALUES (2, 'device-2', 'v1.3-beta/storage', NULL, NULL, NULL, 0);
 
@@ -26,5 +26,62 @@ VALUES
 -- Compliance Records
 INSERT INTO compliance_record (run_group_id, requester_id, created_at)
 VALUES
-(1, 1, '2025-11-24T12:30Z')
+(1, 1, '2025-11-24T12:30Z');
 
+-- Compliance Requests
+INSERT INTO compliance_request (created_at, created_by, updated_at, updated_by, status, csip_aus_version, witness_test, der_brand, der_oem, der_series, der_representative_models, software_client_type, software_client_providers, software_client_versions, onsite_hardware_details)
+VALUES
+(
+	'2026-05-04T13:15Z',
+	2,
+	'2026-05-04T13:15Z',
+	2,
+	1,
+	'v1.2',
+	'2026-05-01T15:30Z',
+	'der_brand',
+	'der_oem',
+	'der_series',
+	'der_representative_models',
+	'software_client_type',
+	'software_client_providers',
+	'software_client_versions',
+	'onsite_hardware_details'
+),  -- compliance_request_id 1
+(
+	'2026-05-07T09:09Z',
+	2,
+	'2026-05-07T09:09Z',
+	2,
+	1,
+	'v1.2',
+	'2026-05-02T00:00Z',
+	'der_brand',
+	'der_oem',
+	'der_series',
+	'der_representative_models',
+	'software_client_type',
+	'software_client_providers',
+	'software_client_versions',
+	'onsite_hardware_details'
+);  -- compliance_request_id 2
+
+-- Compliance Request Classes
+INSERT INTO compliance_request_class (compliance_request_id, compliance_class)
+VALUES
+(1, 'L'),
+(1, 'DER-A'),
+(1, 'S-G'), -- end of compliance request 1 
+(2, 'L'),
+(2, 'DER-A'),
+(2, 'S-G'); -- end of compliance request 2
+
+-- Compliance Request Runs
+INSERT INTO compliance_request_run (compliance_request_id, compliance_run_id)
+VALUES
+(1, 1),
+(1, 3),
+(1, 5), -- end of compliance request 1
+(2, 1),
+(2, 3),
+(2, 5); -- end of compliance request 2
