3dc45343ce3e67efebb2eccc1708490482797f33
Mike Turner <mike.turner@anu.edu.au>
2026-06-17 13:35:44 +0800

    Add missing alembic migration

--- alembic/versions/63667f661ad5_add_file_data_column_to_.py
+++ alembic/versions/63667f661ad5_add_file_data_column_to_.py
@@ -5,17 +5,17 @@ Revises: 97983bab48cb
 Create Date: 2026-06-17 11:50:45.135370
 
 """
-from typing import Sequence, Union
+from collections.abc import Sequence
 
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = '63667f661ad5'
-down_revision: Union[str, None] = '97983bab48cb'
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+down_revision: str | None = '97983bab48cb'
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
