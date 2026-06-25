27d62dd2d45dc7a400796a2423a8986dc477ab49
Josh Vote <jjvote@gmail.com>
2026-06-25 11:33:31 +1000

    Fixed pod images

--- tests/integration/test_pod.py
+++ tests/integration/test_pod.py
@@ -97,9 +97,9 @@ async def test_spawn_brings_up_healthy_pod_then_destroy_cleans_up(ensure_network
     images = PodImages(
         csip_aus_version=CSIP_AUS_VERSION,
         postgres="docker.io/library/postgres:15",
-        init="cactusimageregistry.azurecr.io/cactus-teststack-init:158-v12",
-        envoy="cactusimageregistry.azurecr.io/cactus-envoy:158-v12",
-        runner="cactusimageregistry.azurecr.io/cactus-runner:158-v12",
+        init="cactusimageregistry.azurecr.io/cactus-teststack-init:163-v12",
+        envoy="cactusimageregistry.azurecr.io/cactus-envoy:163-v12",
+        runner="cactusimageregistry.azurecr.io/cactus-runner:163-v12",
     )
 
     resources = PodResources(
