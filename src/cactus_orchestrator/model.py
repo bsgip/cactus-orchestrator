6c6bd3150fae217737214fc5b5dcd8f2f9a0ce2c
LachlanJW <watsonjlachlan@gmail.com>
2026-08-17 13:00:54 +1000

    wip

--- src/cactus_orchestrator/model.py
+++ src/cactus_orchestrator/model.py
@@ -106,6 +106,7 @@ class RunStatus(IntEnum):
     terminated = auto()  # Run was never explicitly finalised, shutdown somehow.
     provisioning = auto()  # Run has just been created - k8s services will be started next.
     skipped = auto()  # Run was skipped (e.g., playlist started from a later index)
+    finalised_by_completion = auto()  # Run was finalised automatically after the runner reported it had finished
 
 
 class Run(Base):
