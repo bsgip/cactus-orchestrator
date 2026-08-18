31b4ca33b20db0d006205fc4f967e9e7b7636c97
Mike Turner <mike.turner@anu.edu.au>
2026-08-18 08:32:36 +0800

    Fix linting errors

--- alembic/versions/ab03a5b33836_modify_add_columns_of_compliancerequest_.py
+++ alembic/versions/ab03a5b33836_modify_add_columns_of_compliancerequest_.py
@@ -6,17 +6,17 @@ Create Date: 2026-08-17 09:55:22.346089
 
 """
 
-from typing import Sequence, Union
+from collections.abc import Sequence
 
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "ab03a5b33836"
-down_revision: Union[str, None] = "fa77cc530a1d"
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+down_revision: str | None = "fa77cc530a1d"
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
